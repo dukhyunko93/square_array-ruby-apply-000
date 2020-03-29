24528f9241d586679be05486fd6b3af7b1997679
Duke Ko <dukhyunko93@gmail.com>
2020-03-29 19:25:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
   new_array = [ ]
+<<<<<<< HEAD
   array.each {|x|new_array.push x ** 2}
+=======
+  array.each {|numbers| new_array.push numbers ** 2}
+>>>>>>> 5c1786a66ebd679651daaea51a7e6c7a13d4cd1a
   new_array
 end
